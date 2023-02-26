d4a16c466a9f07a76d96dd7b9a7a1b292032882a
Vijay-Daraji <darajivijay542@gmail.com>
2023-02-26 13:12:15 +0530

    unit test using mockito

--- src/test/java/com/vijay/ebook/service/impl/CategoryServiceImplTest.java
+++ src/test/java/com/vijay/ebook/service/impl/CategoryServiceImplTest.java
@@ -1,13 +1,25 @@
 package com.vijay.ebook.service.impl;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+//public class CategoryServiceImplTest {
+//	
+//	
+//	
+//}
 import static org.mockito.Mockito.verify;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.vijay.ebook.entity.Category;
 import com.vijay.ebook.repository.CategoryRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,10 +37,17 @@ public class CategoryServiceImplTest {
 	
 	@Test
 	void getAllCategory() {
-		
+		Category category1 = new Category(101L, "category1",null);
+		Category category2 = new Category(102L, "category2",null);
+		List<Category> list = new ArrayList<>();
+		list.add(category1);
+		list.add(category2);
+		Mockito.when(categoryRepository.findAll()).thenReturn(list);
 		categoryServiceImpl.getAllCategory();
 		
-		verify(categoryRepository).findAll();
+		assertEquals(2, categoryServiceImpl.getAllCategory().getBody().size());
+		
+//		verify(categoryRepository).findAll();
 	}
 
 }
